8bf370a28ae57faa26691bbc9e14801ecc315726
PriyaTsl <priya.k@translab.io>
2025-02-21 09:55:20 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_count" {
+  description = "Number of resources to create"
+  type        = number
+}
+
+variable "command_string" {
+  description = "Command to run"
+  type        = string
+}
